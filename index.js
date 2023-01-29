fda2ce2400eb12ac0270699e2659ae7c1e4b0b6e
Oqua Ntoe <oquantoe@outlook.com>
2023-01-29 06:18:57 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+var http = require("http");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+app.use(express.json());
+
+app.route("/").get((req, res) => {
+  res.json("Hey there welcome to turnt chat")
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log("server started");
+});
